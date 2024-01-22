8f896d648f410f83a4d3aca419fe05a976ef077c
Neidr4 <adrien@eatcala.com>
2024-01-22 10:00:35 +0100

    feat: Add the install package manager for tmux

--- tmux/install_tmux.sh
+++ tmux/install_tmux.sh
@@ -2,4 +2,5 @@ sudo apt install -y tmux
 mkdir -p ~/.config/tmux
 cp ./tmux.conf ~/.config/tmux
 cd ~/.config/tmux
-# echo "Open tmux and press Ctrl-B and Shift-I to install the plugins"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Open tmux and press Ctrl-B and Shift-I to install the plugins"
